2c7e2117a68365071e5b84f3e7ef47eefdf660ec
wonil112 <nicewonil@naver.com>
2024-08-12 15:52:53 +0900

    initial project

---
+++ src/main/java/com/continewbie/guild_master/GuildMasterApplication.java
@@ -0,0 +1,13 @@
+package com.continewbie.guild_master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuildMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuildMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/continewbie/guild_master/GuildMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.continewbie.guild_master;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuildMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
