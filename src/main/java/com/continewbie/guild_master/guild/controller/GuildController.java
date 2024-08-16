554fc6868ebb3f4f58fc5f165384878e1f1a4f2c
Gizmoring <arcanacoach@gmail.com>
2024-08-17 01:58:53 +0900

    길드

---
+++ src/main/java/com/continewbie/guild_master/guild/controller/GuildController.java
@@ -0,0 +1,14 @@
+package com.continewbie.guild_master.guild.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/guilds")
+@Validated
+@Slf4j
+public class GuildController {
+
+}
---
+++ src/main/java/com/continewbie/guild_master/guild/dto/GuildDto.java
@@ -0,0 +1,10 @@
+package com.continewbie.guild_master.guild.dto;
+
+import lombok.Getter;
+
+public class GuildDto {
+    @Getter
+    public static class Post{
+
+    }
+}
